9dba91cabb8a4ca65cd554777a2df5a76cf3a844
Carloskaa <ccarom@unal.edu.co>
2023-03-30 17:47:16 -0500

    add tipos

--- bd.py
+++ bd.py
@@ -12,7 +12,8 @@ import pandas as pd
 from io import BytesIO
 from Clases import Conexion_google_sheets
 from PIL import Image
-
+import plotly.express as px
+import plotly.graph_objs as go
 st.set_page_config(page_title="Casablanca",page_icon=Image.open("Logo.ico"))
 
 # with open('style.css') as f:
@@ -78,29 +79,61 @@ if selected == "Home":
     b2.metric("Mes",'$'+'{:,}'.format(df_ingreso.groupby(df_ingreso['fecha'].dt.month)['Costo'].sum()[now.month]), "0%")
     b3.metric("Dia",'$'+'{:,}'.format(df_ingreso['Costo'][df_ingreso['fecha'].dt.strftime('%d-%m-%Y')== now.strftime("%d-%m-%Y")].sum()), "0%")
     
+    
+    # df['Precio'] = pd.to_numeric(df['Precio'].str.replace('[^\d.]', ''))
+    gastostipo = pd.pivot_table(df_gastos, values='Costo', columns=['Tipo'], aggfunc=sum)
+    gastospago = pd.pivot_table(df_gastos, values='Costo', columns=['pago'], aggfunc=sum)
+    
+    # fig = go.Figure(data=[go.Table(
+    # header=dict(values=list(gastostipo.columns),
+    #             fill_color='paleturquoise',
+    #             align='left'),
+    # cells=dict(values=[gastostipo['Casa']],
+    #            fill_color='lavender',
+    #            align='left'))])
+
+    # st.plotly_chart(fig)
+    
+    # fig = go.Figure(data=[go.Table(
+    # header=dict(values=list(gastospago.columns),
+    #             fill_color='paleturquoise',
+    #             align='left'),
+    # cells=dict(values=[gastospago['Casa']],
+    #            fill_color='lavender',
+    #            align='left'))])
+
+    # st.plotly_chart(fig)
+    
+    
+    
+    
 if selected == "Gastos":
     st.write("Ingrese gasto nuevo")
     lista_tipos = ["Muebles","Casa","Tapiceria"]
     lista_tipos.insert(0, "-")
+    lista_pagos = ["Efectivo","Bancolombia Casablanca","Bancolombia Carlos","Davivienda Carlos"]
+    lista_pagos.insert(0, "-")
     if st.checkbox("Agregar gasto"):
         
-        col1, col2, col3= st.columns(3)
+        col1, col2, col3, col4 = st.columns(4)
         with col1:
             gasto = st.text_input('Gasto',"-")
         with col2:
             costo = st.text_input('Costo',"-")
         with col3:
             tipo=st.selectbox("Tipo", ((lista_tipos)))
+        with col4:
+            pago=st.selectbox("Pago", ((lista_pagos)))
             
         if st.button("Agregue registro"):
-            if tipo == "-" or gasto == "-" or costo == "-":
+            if tipo == "-" or gasto == "-" or costo == "-" or pago == "-":
                 st.error('Por favor ingrese datos')
             else:
                 connection = Conexion_google_sheets()
                 lista= connection.conexion_sheets('1Zf73U-ERuCjlRe13_APyY7sCjzXzZX6kVaXJfU51gdI','gastos')
                 now = datetime.now()
                 gs = lista[1]
-                df = pd.DataFrame({'Gasto': [gasto], 'Costo':[float(costo)],  'Tipo': [tipo],'fecha':[now.strftime("%d/%m/%Y, %H:%M:%S")]})
+                df = pd.DataFrame({'Gasto': [gasto], 'Costo':[float(costo)],  'Tipo': [tipo],'Pago': [pago],'fecha':[now.strftime("%d/%m/%Y, %H:%M:%S")]})
                 gs.values_append('gastos', {'valueInputOption': 'RAW'}, {'values': df.values.tolist()})
                 st.success('Registro insertado')
 
@@ -113,23 +146,27 @@ if selected == "Gastos":
         df_gastos['fecha'] = pd.to_datetime(df_gastos['fecha'],format="%d/%m/%Y, %H:%M:%S",errors='coerce')
         df_gastos['fecha'] = df_gastos['fecha'].dt.strftime('%d-%m-%Y, %H:%M')
         df_gastos.loc[:, 'Precio'] ='$'+ df_gastos['Costo'].map('{:,.0f}'.format)
-        dff =df_gastos[['Gasto', 'Precio', 'Tipo', 'fecha']]
+        dff =df_gastos[['Gasto', 'Precio', 'Tipo','pago' ,'fecha']]
         st.dataframe(dff)
         st.download_button(label='📥 Descargar DATAFRAME GENERADO', data=to_excel(df_gastos) ,file_name= "df_gastos.xlsx")
         
 if selected == "Ingresos":
     st.write("Ingrese ingreso de dinero nuevo")
-    lista_persona = ["carlos","andres","fer"]
+    lista_persona = ["Carlos","Henry A","Marlenciaga","Davidcito"]
     lista_persona.insert(0, "-")
+    lista_medios = ["Efectivo","Consignacion Bancolombia","Consignacion Davivienda"]
+    lista_medios.insert(0, "-")
     if st.checkbox("Agregar ingreso"):
         
-        col1, col2, col3= st.columns(3)
+        col1, col2, col3, col4= st.columns(4)
         with col1:
             ingreso = st.text_input('Ingreso',"-")
         with col2:
             costo = st.text_input('Costo',"-")
         with col3:
             persona=st.selectbox("Persona", ((lista_persona)))
+        with col4:
+            medio=st.selectbox("Medio", ((lista_medios)))
             
         if st.button("Agregue registro"):
             if persona == "-" or ingreso == "-" or costo == "-":
@@ -139,7 +176,7 @@ if selected == "Ingresos":
                 lista= connection.conexion_sheets('1Zf73U-ERuCjlRe13_APyY7sCjzXzZX6kVaXJfU51gdI','ingresos')
                 now = datetime.now()
                 gs = lista[1]
-                df = pd.DataFrame({'Ingreso': [ingreso], 'Costo':[float(costo)], 'Persona': [persona],'fecha':[now.strftime("%d/%m/%Y, %H:%M:%S")]})
+                df = pd.DataFrame({'Ingreso': [ingreso], 'Costo':[float(costo)], 'Persona': [persona], 'Medio': [medio],'fecha':[now.strftime("%d/%m/%Y, %H:%M:%S")]})
                 gs.values_append('ingresos', {'valueInputOption': 'RAW'}, {'values': df.values.tolist()})
                 st.success('Registro insertado')
 
@@ -152,7 +189,7 @@ if selected == "Ingresos":
         df_gastos['fecha'] = pd.to_datetime(df_gastos['fecha'],format="%d/%m/%Y, %H:%M:%S",errors='coerce')
         df_gastos['fecha'] = df_gastos['fecha'].dt.strftime('%d-%m-%Y, %H:%M')
         df_gastos.loc[:, 'Precio'] ='$'+ df_gastos['Costo'].map('{:,.0f}'.format)
-        dff =df_gastos[['Ingreso', 'Precio', 'Persona', 'fecha']]
+        dff =df_gastos[['Ingreso', 'Precio', 'Persona','Medio','fecha']]
         st.dataframe(dff)
         st.download_button(label='📥 Descargar DATAFRAME GENERADO', data=to_excel(df_gastos) ,file_name= "df_gastos.xlsx")
 
